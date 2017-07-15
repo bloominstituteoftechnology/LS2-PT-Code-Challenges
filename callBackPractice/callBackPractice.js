63732514a1cae20acb434467d365549ae9c1b78b
bobby brady <bloopsocket@gmail.com>
2017-07-15 17:34:02 +0000

    callback practice, progress

--- callBackPractice/callBackPractice.js
+++ callBackPractice/callBackPractice.js
@@ -18,19 +18,33 @@
 
 // Write a function called firstItem that passes the first item of the given array to the callback function
 
+const firstItem = (item, cb) => {
+  cb(item.shift());
+};
+
 const foods = ['pineapple', 'mango', 'ribeye', 'curry', 'tacos', 'ribeye', 'mango'];
 
 firstItem(foods, (firstItem) => {
   console.log(`The first item is ${firstItem}.`);
 });
 
+
+
 // Write a function called getLength that passes the length of the array into the callback
+const getLength = (item, cb) => {
+  cb(item.length);
+};
 
 getLength(foods, (length) => {
   console.log(`The length of the array is ${length}.`);
 });
 
+
 // Write a function called last which passes the last item of the array into the callback
+
+const last = (item, cb) => {
+  cb(item.pop());
+};
 
 last(foods, (lastItem) => {
   console.log(`The last item in the array is ${lastItem}.`);
@@ -38,10 +52,15 @@ last(foods, (lastItem) => {
 
 // Write a function called sumNums that adds two numbers and passes the result to the callback
 
+/*
 
 sumNums(5, 10, (sum) => {
   console.log(`The sum is ${sum}.`);
 });
+
+*/
+
+/*
 
 // Write a function called multiplyNums that adds two numbers and passes the result to the callback
 
@@ -58,6 +77,13 @@ contains(foods, 'ribeye', (result) => {
 
 // Write a function called removeDuplicates that removes all duplicate values from the given array.
 // Pass the array to the callback function.  Do not mutate the original array.
+*/
+const removeDuplicates = (item, cb) => {
+  let unique = item.filter(function(elem, index, self) {
+      return index == self.indexOf(elem);
+});
+  cb(unique);
+};
 
 removeDuplicates(foods, (uniqueFoods) => {
   console.log(`foods with duplicates removed: ${uniqueFoods}`);
@@ -66,6 +92,10 @@ removeDuplicates(foods, (uniqueFoods) => {
 // Write a function called forEach that iterates over the provided array and passes the value and index into the callback.
 
 
+/*
+
 forEach(foods, (value, index) => {
   console.log(`${value} is at index ${index}.`);
 });
+
+*/
