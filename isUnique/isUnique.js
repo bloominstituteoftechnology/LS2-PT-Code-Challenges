e29d1692dbabd392f67355f1ae214e8fb7259ac4
Colin Gage <colin.m.gage@gmail.com>
2017-07-25 19:13:49 -0700

    Complete isUnique.js

--- isUnique/isUnique.js
+++ isUnique/isUnique.js
@@ -4,7 +4,12 @@
  */
 
 const isUnique = (str) => {
-
+	const set = new Set();
+	for (let i = 0; i < str.length; i++) {
+		if (set.has(str[i])) return false;
+		set.add(str[i]);
+	}
+	return true;
 };
 
 console.log(isUnique('abcdhijklmnopqrstuv')); // true
