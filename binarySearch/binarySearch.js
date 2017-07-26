f16f8487d839168dcdb7284691e42fc6ff05b3bb
Gregory Knudsen <dev.artist15@gmail.com>
2017-07-25 21:12:58 -0400

    binary search complete

--- binarySearch/binarySearch.js
+++ binarySearch/binarySearch.js
@@ -12,4 +12,27 @@
 
 const binarySearch = (nums, target) => {
 
+  let lowIndex = 0;
+	let highIndex = nums.indexOf(nums[nums.length - 1]);
+  if (target === nums[highIndex]) {
+    return highIndex;
+  } else if (target === nums[lowIndex]) {
+    return lowIndex;
+  }
+
+	while (lowIndex <= highIndex) {
+		let middleIndex = Math.floor((highIndex + lowIndex) / 2);
+		if (nums[middleIndex] < target) {
+			lowIndex = middleIndex
+		} else if (nums[middleIndex] > target) {
+			highIndex = middleIndex;
+		} else if (nums[middleIndex] === target) {
+			return middleIndex;
+		}
+
+	}
 };
+
+console.log(binarySearch([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 7) === 6); // true
+console.log(binarySearch([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 2) === 1); // true
+console.log(binarySearch([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 10) === 9); // true
