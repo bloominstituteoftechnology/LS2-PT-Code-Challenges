47c3147e6255b6e0ae86193cab5116806e9c188b
Sandeep Chandran <deepsun80@yahoo.com>
2017-07-25 20:12:43 -0500

    Code Challenge binarySearch complete

--- binarySearch/binarySearch.js
+++ binarySearch/binarySearch.js
@@ -5,11 +5,10 @@
  * https://en.wikipedia.org/wiki/Binary_search_algorithm
  * */
 
-/**
- *  * const index = binarySearch([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 2);
- *	* console.log(index); // 1
-**/
-
 const binarySearch = (nums, target) => {
-
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] === target) return i;
+    }
 };
+const index = binarySearch([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 10);
+console.log(index); // 1
