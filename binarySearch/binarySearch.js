8cda1b373cee6efbdaca48aaa20e2c47e3364100
aaren-george <aarengeorge@me.com>
2017-07-25 21:11:13 -0400

    LSPT2 code challenge
    binarySearch

---
+++ binarySearch/binarySearch.js
@@ -0,0 +1,33 @@
+/*
+ * Given a sorted array, find the index of the specified element
+ * using binary search.
+ * https://www.khanacademy.org/computing/computer-science/algorithms/binary-search/a/binary-search
+ * https://en.wikipedia.org/wiki/Binary_search_algorithm
+ * */
+
+/**
+ *  * const index = binarySearch([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 2);
+ *	* console.log(index); // 1
+**/
+
+const binarySearch = (nums, target) => {
+  var minIndex = 0;
+  var maxIndex = nums.length - 1;
+  var currentIndex;
+  var currentElement;
+
+  while (minIndex <= maxIndex) {
+      currentIndex = (minIndex + maxIndex) / 2 | 0;
+      currentElement = nums[currentIndex];
+
+      if (currentElement < target) {
+          minIndex = currentIndex + 1;
+      }
+      else if (currentElement > target) {
+          maxIndex = currentIndex - 1;
+      }
+      else {
+          return currentIndex;
+      }
+  }
+};
