8947dd7ff19d58e40a17cdfc31521e70180a67e0
satishvattikuti <satishvattikuti94@gmail.com>
2017-07-25 21:39:12 -0400

    done

--- binarySearch/binarySearch.js
+++ binarySearch/binarySearch.js
@@ -11,5 +11,17 @@
 **/
 
 const binarySearch = (nums, target) => {
-
+  let min = 0;
+  let max = nums.length - 1;
+  while(max >= min) {
+  let mid = Math.round((min + max)/ 2);
+  if(target === nums[mid]) return mid;
+  else if(nums[mid] < target) min = mid + 1 ;
+  else max = mid - 1;
+  }
+  console.log('The target doesnt exist');
 };
+
+const index = binarySearch([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 2);
+
+console.log(index);
