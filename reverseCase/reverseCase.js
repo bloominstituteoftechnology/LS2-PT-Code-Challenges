a69498c99da924c4e966d80554dfa5257a308ff9
khamp19 <khampton119@gmail.com>
2017-07-29 13:02:42 -0400

    reverse case completed

---
+++ reverseCase/reverseCase.js
@@ -0,0 +1,21 @@
+/*
+ * Write a function that reverses the case of each letter in the strings that it receives.
+ * Example: 'Hello World' -> 'hELLO wORLD'
+ * Assume that each string will contain only spaces and letters.
+ */
+
+reverseCase = (str) => {
+  let newStr = [];
+  for (let i = 0; i <str.length; i++){
+    let letter = str.charAt(i);
+    if (letter === letter.toUpperCase()) {
+      newStr.push(letter.toLowerCase());
+    } else {
+      if (letter === letter.toLowerCase()) {
+      newStr.push(letter.toUpperCase());
+      }
+    }
+  }
+  newStr = newStr.join("");
+  return newStr;
+}
