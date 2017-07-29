e4449789ea8b479c355d6183a25709ec80e31043
Gregory Knudsen <dev.artist15@gmail.com>
2017-07-29 12:39:07 -0400

    use charAt() rather than turn string into array like previous effort in full time class

--- reverseCase/reverseCase.js
+++ reverseCase/reverseCase.js
@@ -3,3 +3,25 @@
  * Example: 'Hello World' -> 'hELLO wORLD'
  * Assume that each string will contain only spaces and letters.
  */
+
+const reverseCase = (str) => {
+    // const arr = str.split('');
+    // let resultStr = '';
+    // arr.map((letter) =>
+    //     letter === letter.toUpperCase() ? resultStr += letter.toLowerCase() :
+    //     resultStr += letter.toUpperCase()).join('');
+    // console.log(arr);
+    // return resultStr;
+
+    let resultString = '';
+    for (let i = 0; i < str.length; i++) {
+        let letter = str.charAt(i);
+        letter === letter.toUpperCase() ? resultString += letter.toLowerCase() :
+            resultString += letter.toUpperCase();
+    }
+    return resultString;
+};
+
+console.log(reverseCase('Hello World') === 'hELLO wORLD'); // true
+console.log(reverseCase('How Are You Today') === 'hOW aRE yOU tODAY'); // true
+console.log(reverseCase('AAAAAAAAh') === 'aaaaaaaaH'); // true
