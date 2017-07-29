319fd66d3bb125707fecd77fedfd13908fc6130f
Matt Higbee <mhigbee@hotmail.com>
2017-07-29 10:24:57 -0600

    finished up challenege

--- reverseCase/reverseCase.js
+++ reverseCase/reverseCase.js
@@ -3,3 +3,19 @@
  * Example: 'Hello World' -> 'hELLO wORLD'
  * Assume that each string will contain only spaces and letters.
  */
+const reverseCase = (string) => {
+  let newString = '';
+  for (let i =0; i < string.length; i++) {
+    if(string[i] === string[i].toUpperCase()) {
+      newString += string[i].toLowerCase();
+    } else if (string[i] === string[i].toLowerCase()) {
+      newString += string[i].toUpperCase();
+    } else {
+      newString += string[i];
+    }
+  }
+  return newString;
+};
+
+let result = reverseCase('Hi Friend');
+console.log(result);
