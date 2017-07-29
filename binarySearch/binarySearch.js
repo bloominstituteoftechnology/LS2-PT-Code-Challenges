18b31206af9242604a5303aaa0799ccccc949096
satishvattikuti <satishvattikuti94@gmail.com>
2017-07-29 12:12:26 -0400

    finished it

--- binarySearch/binarySearch.js
+++ binarySearch/binarySearch.js
@@ -22,6 +22,6 @@ const binarySearch = (nums, target) => {
   console.log('The target doesnt exist');
 };
 
-const index = binarySearch([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 2);
+const index = binarySearch([1, 2, 3, 4, 5, 6, 7, 8, 9, 10,11,13,14], 13);
 
 console.log(index);
