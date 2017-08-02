6135c80811c65b748d10fe7b3861c0c371ec7ef8
Vance Villanueva <vancervillanueva@gmail.com>
2017-08-01 18:40:24 -0700

    challenge problem remove duplicates

--- removeDuplicates/removeDuplicates.js
+++ removeDuplicates/removeDuplicates.js
@@ -4,5 +4,20 @@
  *
  */
 const removeDuplicates = (arr) => {
-	//code here...
+   let newHash = {};
+ 
+    for(let i=0; i< arr.length; i++){ //Loop over all characters of the string
+        if (newHash[arr[i]] != null){ 
+            newHash[arr[i]] = 1; 
+        } else {
+            newHash[arr[i]] = 0;
+        }
+    }
+    
+	return Object.keys(newHash) ;
 };
+
+const data = [1, 1, 1, 2, 2, 3, 4, 5, 5];
+
+
+console.log(removeDuplicates(data));
