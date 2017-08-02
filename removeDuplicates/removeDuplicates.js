c84ba2ffd1a30c4539f23d63f0d902493a823392
Sandeep Chandran <deepsun80@yahoo.com>
2017-08-01 21:07:04 -0500

    working removeDuplicates

--- removeDuplicates/removeDuplicates.js
+++ removeDuplicates/removeDuplicates.js
@@ -4,5 +4,10 @@
  *
  */
 const removeDuplicates = (arr) => {
-	//code here...
-};
+	const newArr = [];
+	for (var i = 0; i < arr.length; i++) {
+    	if (arr[i + 1] !== arr[i]) newArr.push(arr[i]);
+    }
+	return newArr;
+}
+console.log(removeDuplicates([1, 1, 1, 2, 2, 3, 4, 5, 5]));
