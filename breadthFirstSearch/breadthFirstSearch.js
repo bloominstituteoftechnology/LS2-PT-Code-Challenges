e1afecdce84e426beee434488d6203a6bcf0d62f
Noah Yamamoto <egrodo1@gmail.com>
2017-08-05 16:50:34 -0600

    Recursion is hard, I learned. I am fairly confident this is the final version

--- breadthFirstSearch/breadthFirstSearch.js
+++ breadthFirstSearch/breadthFirstSearch.js
@@ -1,46 +1,42 @@
-let value, keys;
-let flag = false;
-let count = 0;
-let traverseMe = [];
+const recursMe = (tree, searchTerm) => {
+  // Set default variables.
+  let value, keys;
+  let flag = false;
+  let traverseMe = [];
+
+  keys = Object.keys(tree);
+  for (let i = 0; i < keys.length && flag !== true; i++) { // Loop so through the keys so long as we haven't found it already.
+    value = tree[keys[i]];
+    if (value === searchTerm) {
+      let flag = true;
+      return flag;
+    } else if (typeof(value) === 'object') { // If we're dealing with a multi-nested value, we need to mark it for traversal.
+      traverseMe.push(value);
+    }
+  }
+
+  // If there are any branches and we havne't found it yet, go one branch lower.
+  if (traverseMe.length !== 0 && flag !== true) {
+    traverseMe.forEach((tree) => {
+       if (recursMe(tree, searchTerm)) { // If we found the item during recursion
+         flag = true;
+         return flag; // Return true.
+       }
+    })
+  }
+  return flag; // If we've reached the bottom that means we've exhausted our branch. Return result.
+}
+
 const tree = {
  x: 1,
- y: 1,
+ y: 3,
  z: {
-	x: 1,
+	x: 2,
 	y: 1,
 	z: 4,
  },
  a: 5,
 };
 
-const recursMe = (tree, searchTerm) => { // O(n) linear complexity.
-  count++
-  console.log(`Searching through layer ${count}`);
-  traverseMe = []; // Clear traverseMe on each iteration of the recursion.
-  keys = Object.keys(tree); // Make array of tree keys.
-  for (let i = 0; i < keys.length; i++) { // Loop through the current layer of the tree, then pass in the next layer as a tree.
-    value = tree[keys[i]];
-
-    if (value === searchTerm) {
-      console.log(`${value} is our value!`);
-      flag = true;
-    }
-    console.log(`${value} is not our value.`);
-    if (typeof(value) === 'object') { // If we're dealing with a multi-nested value, we need to mark it for traversal.
-      traverseMe.push(value); // Add it to the list to get traversed.
-      console.log(`${value} is an object, marking to traverse`);
-      // BUG Problem is that I might need to create a new traverseMe for each level so we don't traverse over things in a higher queue.
-      // I am going to try and address this by clearing traverseMe on each recurse, but I'm not sure if the *higher* up version of the
-      // traverseMe variable will keep their states if I do so. EDIT: It seems that they do indeed, woo!
-    }
-  } // If reaches here without returning true, need to go to next layer.
-  if (traverseMe.length !== 0) { // If we have more to traverse, do so.
-    traverseMe.forEach((tree) => {
-      console.log(`Traversing into ${tree}.`);
-      recursMe(tree, searchTerm);
-    })
-  }// If we have no more to traverse and we haven't yet returned true that means it's false.
-  return flag;
-}
-
-recursMe(tree, 99) ? console.log("It is in there!") : console.log("It is not in there :(");
+const searchTerm = 5;
+recursMe(tree, searchTerm) ? console.log(`${searchTerm} was found.`) : console.log(`${searchTerm} was not found.`);
