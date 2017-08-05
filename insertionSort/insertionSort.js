a0f29ecf1577b3216f6593ac6c37766f9523285f
Gregory Knudsen <dev.artist15@gmail.com>
2017-08-05 13:01:36 -0400

    push to pull

--- insertionSort/insertionSort.js
+++ insertionSort/insertionSort.js
@@ -10,7 +10,20 @@
 // Example usage:
 // insertionSort([2, 1, 3, 7, 4, 2, 9, 3, 8]); // yields [1, 2, 2, 3, 3, 4, 7, 8, 9]
 
-const insertionSort = (array) => {
-  // Your code goes here. Feel free to add helper functions if needed.
+// const insertionSort = (array) => {
+ const array = [9, 2, 5, 6, 4, 3, 7, 10, 1, 8];
+
+function insertionSort(array) {
+  for(let i = 0; i < array.length; i++) {
+    let temp = array[i];
+    let j = i - 1;
+    while (j >= 0 && array[j] > temp) {
+      array[j + 1] = array[j];
+      j--;
+    }
+    array[j + 1] = temp;
+  }
   return array;
-};
+}
+
+console.log(insertionSort(array));
