959efc3845a11f475d0133f3c344280ebaff7da3
Noah Yamamoto <egrodo1@gmail.com>
2017-08-05 12:34:04 -0600

    Added isUnique and updated some comments in BFS

--- breadthFirstSearch/breadthFirstSearch.js
+++ breadthFirstSearch/breadthFirstSearch.js
@@ -18,7 +18,6 @@
 // First move horizonatally across all nodes of current layer before moving onto next layer.
 
 let value, keys, count = 0, traverseMe = [];
-const searchTerm = 2;
 const tree = {
  x: 1,
  y: 1,
@@ -27,14 +26,14 @@ const tree = {
 	y: 1,
 	z: 5,
  },
- a: 2,
+ a: 5,
 };
 
-const recursMe = (tree, searchTerm) => {
+const recursMe = (tree, searchTerm) => { // O(n) linear complexity.
   count++
   console.log(`Searching through layer ${count}`);
-  traverseMe = []; // Clear traverseMe on this iteration of the recursion.
-  keys = Object.keys(tree);
+  traverseMe = []; // Clear traverseMe on each iteration of the recursion.
+  keys = Object.keys(tree); // Make array of tree keys.
   for (let i = 0; i < keys.length; i++) { // Loop through the current layer of the tree, then pass in the next layer as a tree.
     value = tree[keys[i]];
     if (value === searchTerm) console.log(`${value} is our value!`); return true;
@@ -44,7 +43,7 @@ const recursMe = (tree, searchTerm) => {
       console.log(`${value} is an object, marking to traverse`);
       // BUG Problem is that I might need to create a new traverseMe for each level so we don't traverse over things in a higher queue.
       // I am going to try and address this by clearing traverseMe on each recurse, but I'm not sure if the *higher* up version of the
-      // traverseMe variable will keep their states if I do so.
+      // traverseMe variable will keep their states if I do so. EDIT: It seems that they do indeed, woo!
     }
   } // If reaches here without returning true, need to go to next layer.
   if (traverseMe.length !== 0) { // If we have more to traverse, do so.
--- isUnique/isUnique.js
+++ isUnique/isUnique.js
@@ -4,8 +4,36 @@
  */
 
 const isUnique = (str) => {
+  // Non extra credit: Make an object with each letter as a key. If key exists already then not unique. O(n)
+  // Could also use set.
+  let letters = {};
+  for (let i = 0; i < str.length; i++) {
+    if (letters[str[i]] !== undefined) return false;
+    letters[str[i]];
+  }
+  return true;
+};
 
+const isUniqueExtra = (str) => {
+  // Extra credit: Take alphabet, if letter exists in alphabet, remove it. If it doesn't exist it's a dup.
+  let alphabet = 'abcdefghijklmnopqrstuvwxyz';
+  let curr;
+  let flag = true;
+
+  for (let i = 0; i < str.length; i++) {
+    curr = str[i];
+    if (alphabet.indexOf(str[i]) !== -1) {
+      alphabet = alphabet.replace(curr, '');
+      flag = true;
+    } else {
+      flag = false;
+    }
+  }
+  return flag;
 };
 
 console.log(isUnique('abcdhijklmnopqrstuv')); // true
 console.log(isUnique('abcdefga')); // false
+
+console.log(isUniqueExtra('abcdhijklmnopqrstuv')); // true
+console.log(isUniqueExtra('abcdefga')); // false
