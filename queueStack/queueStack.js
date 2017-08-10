d0dedc110e102eb2a58124bd1d3c19d64b5b935f
Gregory Knudsen <dev.artist15@gmail.com>
2017-08-09 21:21:58 -0400

    thanks to Stack Overflow, queueStack complete...pretty cool

--- queueStack/queueStack.js
+++ queueStack/queueStack.js
@@ -2,3 +2,41 @@
  * Write a stack class (you did this in the Data Structures I sprint).
  *  Once you're done, implement a queue using two stacks.
  */
+
+class Queue {
+  constructor(size = 0) {
+    this.inbox = [];
+    this.outbox = [];
+  }
+
+  enqueue(element) {
+    return this.inbox.push(element);
+  }
+
+  dequeue() {
+    if (this.outbox.length === 0) {
+        return this.outbox.push(this.inbox.pop());
+    }
+    return this.outbox.pop();
+  }
+
+  get size() {
+    return this.store.length;
+  }
+}
+
+let myQueue = new Queue();
+
+myQueue.enqueue(1);
+myQueue.enqueue(2);
+myQueue.enqueue(3);
+myQueue.dequeue();
+
+console.log(myQueue);
+
+myQueue.enqueue(4);
+myQueue.dequeue();
+myQueue.enqueue(5);
+myQueue.dequeue();
+console.log('--------------');
+console.log(myQueue);
