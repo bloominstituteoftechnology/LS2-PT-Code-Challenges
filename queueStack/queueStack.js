4ac8e1a2de725bc964df684d6b96bd70d9269eb9
Matt Higbee <mhigbee@hotmail.com>
2017-08-09 19:40:29 -0600

    finishing up queue

--- queueStack/queueStack.js
+++ queueStack/queueStack.js
@@ -2,3 +2,34 @@
  * Write a stack class (you did this in the Data Structures I sprint).
  *  Once you're done, implement a queue using two stacks.
  */
+
+ class Stack {
+  constructor() {
+    this.input = [];
+    this.output = [];
+  }
+  add(value) {
+    this.input.push(value);
+    
+  }
+  remove() {
+    let inputStackValue;
+    if (this.output[0] === null) {
+      for (let i = 0; i < input.length; i++) {
+        inputStackValue = this.input.pop();
+        this.output.push(inputStackValue);
+        i--;
+      }
+    }
+    return this.output.pop();
+  }
+}
+
+// stack last in first out
+// queue first in first out
+
+// If ( Output Stack is Empty)
+//     pop every element in the Input Stack
+//     and push them to the Output Stack until Input Stack is Empty
+
+// pop from Output Stack
