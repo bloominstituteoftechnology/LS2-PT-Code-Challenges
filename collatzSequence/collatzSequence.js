145fd6133ea08981198255c4053e7b227e052f97
khamp19 <khampton119@gmail.com>
2017-09-06 21:29:21 -0400

    collatzSequence done

--- collatzSequence/collatzSequence.js
+++ collatzSequence/collatzSequence.js
@@ -3,7 +3,7 @@
  * sequence by either:
  * - halving the number if the number is even; or
  * - multiplying the number by three and then adding one is n is odd
- * 
+ *
  * Write a function that will take a number n, and return the Collatz chain for that
  * number. You will stop at one.
  *
@@ -12,8 +12,18 @@
  */
 
 const collatzSequence = (n) => {
-
+  const chain = [];
+  let current = n;
+  chain.push(n);
+  while (current > 1) {
+    if (current % 2 === 0) {
+      current = current / 2;
+    } else {
+      current = current * 3 + 1;
+    }
+    chain.push(current);
+  };
+  return chain;
 };
-
 console.log(collatzSequence(23));
 console.log([23, 70, 35, 106, 53, 160, 80, 40, 20, 10, 5, 16, 8, 4, 2, 1]);
